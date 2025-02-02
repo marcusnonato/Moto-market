323d14e1913f56495383793d0b5049b1ec3bc56b
ViniciusMarcuS2i <marcusononato@gmail.com>
2025-02-02 15:01:51 -0400

    feat: add shadcn sheet component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./_fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 const geistMono = localFont({
   src: "./_fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={` ${geistMono.className}  antialiased`}>{children}</body>
     </html>
   );
 }
