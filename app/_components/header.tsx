ad2462e40a16c8f31517372d54a7cc68c5372611
Marcus Nonato <marcusononato@gmail.com>
2025-08-28 20:25:16 -0400

    feat/ajustes-header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -21,7 +21,7 @@ function Header({ isMenuOpen }: HeaderProps) {
         <Link className="hover:text-primary" href="">
           INÍCIO
         </Link>
-        <Link className="hover:text-primary" href="">
+        <Link className="hover:text-primary" href="/products">
           MOTOS
         </Link>
         <div className="cursor-pointer dark:hidden">
@@ -34,11 +34,14 @@ function Header({ isMenuOpen }: HeaderProps) {
             <Image src="/logo-dark.png" alt="Logo" width={90} height={90} />
           </Link>
         </div>
-        <Link className="hover:text-primary" href="">
+        <Link className="hover:text-primary" href="#localization">
           LOCALIZAÇÃO
         </Link>
-        <Link className="hover:text-primary" href="">
-          CONTATO
+        <Link
+          className="hover:text-primary"
+          href="https://wa.me/5592995263569?text=Ol%C3%A1%2C%20tudo%20bem%3F%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%21"
+        >
+          WHATSAPP
         </Link>
       </div>
       <div className="-mt-4 flex w-full items-center justify-between px-4 lg:hidden">
